7c7022880f87b3c3b6b702ec4e24237516f0ffb2
jerecockrell <jere.cockrell@gmail.com>
2016-02-24 17:18:42 -0700

    still incomplete...

--- public/app.js
+++ public/app.js
@@ -1,31 +1,89 @@
 var TodoList = React.createClass({
-	render: function(){
-		var self = this;
-		var todoList = this.props.allMyData.map(function(t){
-			return (
-			<div className="panel panel-default">	
-				<div className="panel panel-header">
-					{t.name}
-				</div>
-				<div className="panel panel-body">
-					{t.due_date}
-				</div>
-				<div className="panel panel-footer">
-					{t.description}
-					<button className="btn btn-warning" 
-					onClick={self.props.handleDelete}> 
-					delete 
-					</button>
-				</div>
-			</div>
-			)
-		})
+  render: function() {
+    var self = this;
+	
+	var todoList = this.props.allMyData.map(function(t){
+	  return (
+		<div className="panel panel-default">	
+		  <div className="panel-header">
+		    { t.name }
+		  </div>
+		  <div className="panel-body">
+			{ t.description }
+		  </div>
+		  <div className="panel-footer">
+			{ t.dueDate }
+			  <button className="btn btn-warning" 
+			   onClick={self.props.handleDelete.bind(this, t._id)}> 
+			   delete 
+			  </button>
+		  </div>
+		</div>
+	    )
+	  })
 
+	  return (
+	    <div>
+	      <p> { todoList } </p>
+	    </div>
+      )
+	}
+});
+
+var TodoForm = React.createClass({
+  getInitialState: function() {
+    return {
+	  name: '',
+	  description: '',
+	  dueDate: ''
+	}
+  },
+	handleNameChange: function(e) {
+	  this.setState({
+	    name: e.target.value
+	})
+  },
+	handleDescriptionChange: function(e) {
+	  this.setState({
+	    description: e.target.value
+		})
+	},
+	handleDueDateChange: function(e) {
+	  this.setState({
+	    dueDate: e.target.value
+		})
+	},
+	handleForm: function(e){
+		e.preventDefault();
+		var name = this.state.name;
+		var description = this.state.description;
+		var dueDate = this.state.dueDate;
+		this.props.handleSubmit({
+			name: name, description: description, dueDate: dueDate
+		})
+	}
+	render: function() {
 		return (
-			<div>
-			  <p> { todoList } </p>
-			</div>
-			)
+		  <div>
+		    <form onSubmit={this.handleForm} method="" role="form">
+			  <legend>Add New Todo</legend>
+			  
+			  <div className="form-group">
+				<input onChange={this.handleNameChange} value={this.state.name} type="text" className="form-control" placeholder="name">
+			  </div>
+
+			  <div className="form-group">
+			    <input onChange={this.handleDescriptionChange} value={this.state.description} type="text" className="form-control" placeholder="description">
+			  </div>
+			  
+			  <div className="form-group">
+			    <input onChange={this.handleDueDateChange} value={this.state.dueDate} type="text" className="form-control" placeholder="due date">
+			  </div>
+
+			  <button type="submit" className="btn btn-primary">Submit</button>
+		    </form>
+		  </div>
+		)
 	}
 })
 
@@ -49,8 +107,28 @@ var App = React.createClass({
 		});
 	},
 
-	handleDelete: function() {
-		alert("want to delete?")
+	handleSubmit: function() {
+		var self = this;
+		$.ajax({
+		  url: '/api/todo/',
+		  method: 'POST',
+		  data: todo
+		}).done(function(){
+		  self.loadTodosFromServer();
+		  console.log('posted todo to server!')
+	    })
+	}
+
+	handleDelete: function(id) {
+		var id = id;
+		var self = this;
+		$.ajax({
+		  url: '/api/todo/' + id,
+		  method: 'DELETE'
+		}).done(function(){
+		  console.log('deleted todo');
+		  self.loadTodosFromServer();
+		})
 	},
 
 	componentDidMount: function() {
@@ -63,6 +141,7 @@ var App = React.createClass({
 				<h3> Hello World! </h3>
 				<TodoList handleDelete={ this.handleDelete } 
 				          allMyData={ this.state.todos }/>
+				<TodoForm />
 			</div>
 			)
 	}
